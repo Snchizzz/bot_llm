872142d11f2ffc698bcb3a3ed082df8d7bc20e96
Snchizzz <sandrovouba@gmail.com>
2025-11-06 17:47:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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"
+)
