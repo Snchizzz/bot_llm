341bd02163eac3f1d16bdfa8923a4600a73ea480
Snchizzz <sandrovouba@gmail.com>
2025-11-09 01:47:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key="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"
-)
-
